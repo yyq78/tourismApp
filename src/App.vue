6ea2c265edb8a3ba4269cf22f9e0e5137cfa7540
杨玉卿 <1453582072@qq.com>
2019-03-29 21:28:38 +0800

    init ready:add reset.css/fastclick.js/iconfont

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    home
+    <i class="iconfont">&#xe632;</i>
   </div>
 </template>
 
@@ -11,13 +11,3 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -5,7 +5,12 @@ import App from './App'
 import router from './router'
 
 Vue.config.productionTip = false
-
+//css
+import './assets/css/reset.css'
+import './assets/css/iconfont.css'
+//js
+import FastClick from 'fastclick'
+FastClick.attach(document.body);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
