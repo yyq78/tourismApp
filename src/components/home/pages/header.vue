fff1d595226701302b50e0041c81b61a8b7806c2
杨玉卿 <1453582072@qq.com>
2019-04-01 19:17:49 +0800

    头部header组件布局

--- src/components/home/pages/header.vue
+++ src/components/home/pages/header.vue
@@ -1,8 +1,47 @@
 <template>
-  <div>
-      header
+  <div class="header">
+      <div class="header-left">
+        <span class='iconfont'>&#xe600;</span>
+      </div>
+      <div class="header-search">
+        <span class='iconfont'>&#xe632;</span>
+        输入城市/景点/游玩主题
+      </div>
+      <div class="header-right">
+        成都<span class='iconfont'>&#xe607;</span>
+      </div>
   </div>
 </template>
 <script>
 </script>
-<style scoped></style>
+<style scoped>
+  .header{
+    width:100%;
+    line-height:.88rem;
+    background:#00bcd4;
+    color:#fff;
+    font-size:.36rem;
+    display: flex;
+  }
+  .header-search{
+      flex:1;
+      background:#fff;
+      height:.6rem;
+      margin:.14rem 0;
+      border-radius:.1rem;
+      color:#e4e7ea;
+      line-height:.6rem;
+      font-size:.28rem;
+      padding-left:.2rem;
+  }
+  .header-left{
+    width:.4rem;
+    padding:0 .2rem;
+    text-align:center;
+    font-weight:bold;
+  }
+  .header-right{
+    font-size:.28rem;
+    padding:0 .2rem;
+  }
+</style>
