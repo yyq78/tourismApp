93630e673417781904a27c3a85583b5f695aa3c8
杨玉卿 <1453582072@qq.com>
2019-04-04 20:11:21 +0800

    add location

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -3,18 +3,21 @@
       <home-header></home-header>
       <home-swiper></home-swiper>
       <home-icon></home-icon>
+      <home-location></home-location>
   </div>
 </template>
 <script>
 import HomeHeader from './pages/Header'
 import HomeSwiper from './pages/Swiper'
 import HomeIcon from './pages/Icon'
+import HomeLocation from './pages/Location'
 
 export default{
   components:{
     HomeHeader,
     HomeSwiper,
-    HomeIcon
+    HomeIcon,
+    HomeLocation
   }
 }
 </script>
---
+++ src/components/home/pages/Location.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="location border-top">
+
+      <div class="location-item border-right">
+        <span class="iconfont">&#xe63f;</span>
+        四川成都
+      </div>
+      <div class="location-item">
+        <span class="iconfont">&#xe7a8;</span>
+        必游榜单
+      </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+  .location{
+    display:flex;
+    position:relative;
+  }
+  .location .location-item{
+    flex:1;
+    font-size:.28rem;
+    text-align:center;
+    line-height:1rem;
+
+  }
+</style>
