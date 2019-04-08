cf3039de2f076ecb2569585b85fe7e94362916da
杨玉卿 <1453582072@qq.com>
2019-04-08 11:51:11 +0800

    add city.json

--- src/components/city/City.vue
+++ src/components/city/City.vue
@@ -1,28 +1,66 @@
 <template>
   <div>
     <city-header></city-header>
-    <city-hot></city-hot>
-    <city-sort></city-sort>
-    <city-list></city-list>
+
+    <city-list
+     :hotCities="hotCities"
+     :cities="cities">
+     </city-list>
   </div>
 </template>
 
 
 <script>
 import CityHeader from './pages/Header'
-import CityHot from './pages/Hot'
-import CitySort from './pages/Sort'
 import CityList from './pages/List'
 export default{
   components:{
     CityHeader,
-    CityHot,
-    CitySort,
     CityList
+  },
+  data(){
+    return {
+      hotCities:[],
+      cities:{}
+    }
+  },
+  mounted(){
+    this.$http.get('api/city.json')
+    .then((res)=>{
+      const data=res.data.data;
+      this.hotCities=data.hotCities;
+      this.cities=data.cities;
+    })
   }
 }
 </script>
 
 <style>
-  
+   .hot-top{
+    font-size:.24rem;
+    padding:.2rem;
+  }
+  .hot-list{
+    overflow:hidden;
+    position:relative;
+   
+  }
+  .hot-list li{
+    float:left;
+    width:33.33%;
+    font-size:.28rem;
+    line-height:.9rem;
+    text-align: center;
+    border-bottom: .02rem solid #ddd;
+    margin-bottom:-1px;
+  }
+  .hot-list:before{
+    content:" ";
+    position:absolute;
+    left:33.33%;
+    width:33.33%;
+    height:100%;
+    border-right:.02rem solid #ddd;
+    border-left:.02rem solid #ddd;
+  }
 </style>
--- src/components/city/pages/List.vue
+++ src/components/city/pages/List.vue
@@ -1,24 +1,59 @@
 <template>
-  <div class="list">
-    <div>
-      <div class="item-title">A</div>
-      <ul class="item-msg">
-        <li>阿坝</li>
-        <li>阿坝</li>
-        <li>阿坝</li>
-        <li>阿坝</li>
-        <li>阿坝</li>
-        <li>阿坝</li>
-        <li>阿坝</li>
-        <li>阿坝</li>
-      </ul>
+  <div class="wrapper">
+    <div class="content">
+      <!--hot-->
+      <div class="hot">
+        <div class="hot-top">
+          热门城市
+        </div>
+        <ul class="hot-list">
+          <li class="hot-item" v-for="item in hotCities" :key=item.id>
+            {{item.name}}
+          </li>
+        </ul>
+      </div>
+      <!--sort-->
+      <div class="sort">
+        <div class="sort-top">字母排序</div>
+        <ul class="sort-list">
+          <li v-for="(item,key) in cities" @click='changeSort(key)'>{{key}}</li>
+          
+        </ul>
+      </div>
+
+      <!--list-->
+      <div class="list">
+        <div v-for="(val,key) in cities" :ref="key">
+          <div class="item-title">{{key}}</div>
+          <ul class="item-msg">
+            <li v-for="item in val" :key=item.id>{{item.name}}</li>
+          </ul>
+        </div>
+      </div>
     </div>
   </div>
+
 </template>
 
 <script>
+import BScroll from 'better-scroll';
+
 export default {
-  
+  props:["hotCities","cities"],
+  data(){
+    return {
+      scroll:{}
+    }
+  },
+  methods: {
+    changeSort(sortName){
+      
+    }
+  },
+  mounted() {
+    let wrapper = document.querySelector('.wrapper')
+    this.scroll = new BScroll(wrapper);
+  },
 }
 </script>
 
@@ -40,7 +75,7 @@ export default {
     border-bottom: .02rem solid #ddd;
     margin-bottom:-1px;
   }
- .item-msg:before{
+  .item-msg:before{
     content:" ";
     position:absolute;
     left:25%;
@@ -49,12 +84,26 @@ export default {
     border-right:.02rem solid #ddd;
     border-left:.02rem solid #ddd;
   }
-   .item-msg:after{
+  .item-msg:after{
     content:" ";
     position:absolute;
     right:25%;
     width:25%;
     height:100%;
     border-left:.02rem solid #ddd;
+  }
+  .sort-top{
+    font-size:.24rem;
+    padding:.2rem;
+  }
+  .sort-list{
+    overflow:hidden; 
+  }
+  .sort-list li{
+    float:left;
+    width:16.66%;
+    font-size:.28rem;
+    line-height:.9rem;
+    text-align: center;
   }
 </style>
